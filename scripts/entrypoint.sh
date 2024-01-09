a699f13567784a0c3af87cbe1bace2b8748c5cf0
Alison Mukoma <Alison.Mukoma@trivago.com>
2024-01-09 21:53:38 +0100

    disable tools install

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -9,7 +9,7 @@ grunt build
 grunt copy:sources
 grunt build
 
-cd ../backend && python3.9 -m pip install -r requirements.txt 
+cd ../backend && pip install -r requirements.txt 
 cd /var/globaleaks
 
 # sudo ./backend/bin/globaleaks -z -n
